19ea75f67bd55f46dcc78962eff8f1bedbb80d0a
Mahmoud Khaled Gaber <mahmoud_khld@yahoo.com>
2015-09-28 13:07:12 +0200

    Revert "Batch Crawler"
    This reverts commit ad92b6eca7c792827c9dc033e32b39d5a4629ef0.

--- G2A Crawler/crawl-dump.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-curl -s -k "https://www.g2a.com/lucene/item/%1" -H "User-Agent: CMD" >> ProductCrawl\%1.txt
-
-exit
--- G2A Crawler/~crawler-trigger.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-MD ProductCrawl
-
-echo Start?
-pause
-
-FOR /F %%i IN (list-ID-only.org) DO START /B crawl-dump.bat %%i
-exit

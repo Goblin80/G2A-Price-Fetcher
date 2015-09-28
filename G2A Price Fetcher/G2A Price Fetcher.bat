16a10b983a278d68230e07d215f23e60e94f2acc
Mahmoud Khaled Gaber <mahmoud_khld@yahoo.com>
2015-09-28 13:05:53 +0200

    Revert "Masking the user agent"
    This reverts commit 3f62e1f646b6b7f7abc9908cd6d8b71c750c283c.

--- G2A Price Fetcher/G2A Price Fetcher.bat
+++ G2A Price Fetcher/G2A Price Fetcher.bat
@@ -1,16 +1,14 @@
 @echo off
 
 ::==============================================================================================================::
-SET UA=CMD
-
 SET /p NAME=Type Product Name:
 SET  NAME=%NAME: =+%
 
-curl -k -s "https://www.g2a.com/lucene/search/quick?phrase=%NAME%" -H "User-Agent: %UA%" | jq ".docs[] | {Name:.name, Product_ID:.id}"
+curl -k -s "https://www.g2a.com/lucene/search/quick?phrase=%NAME%" | jq ".docs[] | {Name:.name, Product_ID:.id}"
 
 SET /P ID=Type corresponding ID:
 ::==============================================================================================================::
 
-curl -k -s "https://www.g2a.com/marketplace/product/auctions/?id=%ID%" -H "User-Agent: %UA%" | jq ".a[] | .f"
+curl -k -s "https://www.g2a.com/marketplace/product/auctions/?id=%ID%" | jq ".a[] | .f"
 
 pause
